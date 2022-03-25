0d9cc20285aee220866f2d3e92bb231b4fe3b859
kilik42 <marvinevins@yahoo.com>
2022-03-25 05:54:09 -0500

    working on scrollinf functionality

--- src/App.js
+++ src/App.js
@@ -8,13 +8,16 @@ function App() {
 
       <div className="app__top">
             {/* image */}
-
+          <img src="https://imgs.search.brave.com/c4g6-eXJbAgOSVYiUGLILCGQOj87zB5Sj1TqvvcIRR8/rs:fit:1024:1024:1/g:ce/aHR0cDovL3dvcmtp/bmd3aXRoZG9nLmNv/bS93cC1jb250ZW50/L3VwbG9hZHMvMjAx/Ni8wNS9uZXdfaW5z/dGFncmFtX2xvZ28t/MTAyNHgxMDI0Lmpw/Zw" alt="" className="app__logo" />
                   {/* reels text */}
+                  <h1>Reels</h1>
       </div>
 
      <div className="app__videos">
         {/* container of app videos --scrolling container*/}
-        
+
+       {/* videos */}
+
      </div>
 
     
