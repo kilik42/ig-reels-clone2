aeeb6927f755bae525c1e715983f95a363f0bed5
kilik42 <marvinevins@yahoo.com>
2022-03-25 05:47:01 -0500

    working on app setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hey lets program an IG reels clone</h1>
+
+      <div className="app__top">
+            {/* image */}
+
+                  {/* reels text */}
+      </div>
+
+     <div className="app__videos">
+        {/* container of app videos --scrolling container*/}
+        
+     </div>
+
+    
+     
     </div>
   );
 }
