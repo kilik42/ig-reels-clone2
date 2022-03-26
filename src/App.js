6b4896f9395b7bc03de3feaa6e8fd0809cfc37d2
kilik42 <marvinevins@yahoo.com>
2022-03-26 06:31:13 -0500

    working on video card interactivity

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
             {/* image */}
           <img src="https://imgs.search.brave.com/c4g6-eXJbAgOSVYiUGLILCGQOj87zB5Sj1TqvvcIRR8/rs:fit:1024:1024:1/g:ce/aHR0cDovL3dvcmtp/bmd3aXRoZG9nLmNv/bS93cC1jb250ZW50/L3VwbG9hZHMvMjAx/Ni8wNS9uZXdfaW5z/dGFncmFtX2xvZ28t/MTAyNHgxMDI0Lmpw/Zw" alt="" className="app__logo" />
                   {/* reels text */}
-                  <h1>Reels</h1>
+            <h1>Reels</h1>
       </div>
 
      <div className="app__videos">
--- src/VideoCard.js
+++ src/VideoCard.js
@@ -1,10 +1,29 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import './VideoCard.css'
 
 function VideoCard() {
+//usestate hook to keep track of the video card's state
+const [isVideoPlaying, setIsVideoPlaying] = useState(false);
+//ref to the video element its a pointer to the video element
+const videoRef = useRef(null);
+const onVideoPress = () => {
+
+  if (isVideoPlaying) {
+    //stop the video
+    videoRef.current.pause();
+    setIsVideoPlaying(false);
+  
+  }else{
+    //play the video
+    videoRef.current.play();
+    setIsVideoPlaying(true);
+  }
+}
   return (
     <div className="videoCard">
       <video 
+      ref={videoRef}
+      onClick={onVideoPress}
       className="video__player"
       src='https://player.vimeo.com/external/519943720.sd.mp4?s=d9609b546c57dac56dd27752dc62c9a003556042&profile_id=165&oauth2_token_id=57447761'
       alt='IG reel video'
