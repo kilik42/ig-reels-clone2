4e6335dc55da5b0ad370194d983c1143c1fc2e8a
kilik42 <marvinevins@yahoo.com>
2022-03-31 05:22:59 -0500

    working on firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,5 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import firebase from 'firebase';
 const firebaseConfig = {
     apiKey: "AIzaSyDC5VmErk3WRbjgdNyQCmmOQxyTlWn-AU8",
     authDomain: "reelsclone-60856.firebaseapp.com",
@@ -8,3 +9,9 @@ const firebaseConfig = {
     appId: "1:325811160988:web:f9eb1bd6a2d423cf982a59",
     measurementId: "G-LS6NC45RR2"
   };
+  // Initialize Firebase
+  const firebaseApp = firebase.initializeApp()
+  (firebaseConfig);
+  const db = firebaseApp.firestore();
+
+  export default db;
